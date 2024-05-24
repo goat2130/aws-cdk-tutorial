e84a2ec2b1f43e5b91becd4126908fa28d0cdaee
goat2130 <yagisawahodaka@team-lab.com>
2024-05-24 11:02:51 +0900

    Initial commit

---
+++ test/cdk-workshop-constructor.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as CdkWorkshopConstructor from '../lib/cdk-workshop-constructor-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/cdk-workshop-constructor-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new CdkWorkshopConstructor.CdkWorkshopConstructorStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
